c16ff512cb3d17379f738863f84574ae9a45109c
nellyem24 <nellyem24@gmail.com>
2024-08-21 15:49:29 -0500

    clap to manage commands fixed

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,22 @@
-use std::path::PathBuf;
+use clap::Parser;
 
-use clap::{ArgEnum, IntoApp, Parser, Subcommand, ValueHint};
-use clap_complete::{generate, Generator, Shell};
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of the person to greet
+    #[arg(short, long)]
+    name: String,
 
-/// Greet command (example for clap_complete command).
-#[derive(Parser,Debug)]
-struct Cli {
-    #[clap(subcommand)]
-    action: Action,
-}
-
-#[derive(Subcommand,Debug)]
-enum Action {
-    /// Greet some message.
-    Greet {
-        /// Language in which messages are shown.
-        #[clap(long,short,arg_enum)]
-        language: Option<Language>,
-        /// File whose content is printed.
-        ///
-        /// The trailing whitespaces of the content are trimmed.
-        #[clap(long,short,conflicts_with("language"),value_hint(ValueHint::FilePath))]
-        file: Option<PathBuf>,
-    },
-    /// Generate completion script
-    Completion {
-        #[clap(long,short,arg_enum)]
-        shell: Shell,
-    },
-}
-
-#[derive(ArgEnum,Clone,Debug)]
-enum Language {
-    En,
-    Ja,
-}
-
-impl Action {
-    fn handle(self) {
-        use Action::*;
-
-        match self {
-            Greet { language: None, file: None } => {
-                println!("Hello");
-            },
-            Greet { language: Some(Language::En), .. } => {
-                println!("Hello");
-            },
-            Greet { language: Some(Language::Ja), .. } => {
-                println!("こんにちは");
-            },
-            Greet { file: Some(file), .. } => {
-                let s = std::fs::read_to_string(&file).unwrap();
-                println!("{}", s.trim_end());
-            },
-            Completion { shell } => {
-                print_completer(shell);
-            },
-        }
-    }
-}
-
-fn print_completer<G: Generator>(generator: G) {
-    let mut app = Cli::into_app();
-    let name = app.get_name().to_owned();
-
-    generate(generator, &mut app, name, &mut std::io::stdout());
+    /// Number of times to greet
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
 }
 
 fn main() {
-    Cli::parse().action.handle();
+    let args = Args::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name);
+    }
 }
