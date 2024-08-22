075f125f4f6efcba487a96f7cff4f7e8487f367e
nellyem24 <nellyem24@gmail.com>
2024-08-22 00:04:00 -0500

    added clap and first command

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,41 @@
-use clap::Parser;
+fn main() {
+    let cmd = clap::Command::new("uni")
+        .bin_name("uni")
+        .styles(CLAP_STYLING)
+        .subcommand_required(true)
+        .subcommand(
+            clap::command!("example").arg(
+                clap::arg!(--"manifest-path" <PATH>)
+                    .value_parser(clap::value_parser!(std::path::PathBuf)),
+            ),
+        )
+        .subcommand(clap::command!("bycountry"));
 
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    let matches = cmd.get_matches();
+    let matches = match matches.subcommand() {
+        Some(("example", matches)) => matches,
+        Some(("bycountry", _)) => {
+            bycountry();
+            return;
+        }
+        _ => unreachable!("clap should ensure we don't get here"),
+    };
+    let manifest_path = matches.get_one::<std::path::PathBuf>("manifest-path");
+    println!("{manifest_path:?}");
 }
 
-fn main() {
-    let args = Args::parse();
+pub const CLAP_STYLING: clap::builder::styling::Styles = clap::builder::styling::Styles::styled()
+    .header(clap_cargo::style::HEADER)
+    .usage(clap_cargo::style::USAGE)
+    .literal(clap_cargo::style::LITERAL)
+    .placeholder(clap_cargo::style::PLACEHOLDER)
+    .error(clap_cargo::style::ERROR)
+    .valid(clap_cargo::style::VALID)
+    .invalid(clap_cargo::style::INVALID);
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name);
-    }
+fn bycountry() {
+    // TODO: read csv 
+    
+    // get the top 10 countries with more startups
+    println!("bycountry");
 }
